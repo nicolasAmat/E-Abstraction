02522439a313b7c053f8b37354e4ebe1ce2d34cb
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2020-10-21 11:43:08 +0200

    Add indents

--- Petri_Net.thy
+++ Petri_Net.thy
@@ -43,12 +43,12 @@ definition resulting_marking_firing_transition :: "('pl,'tr,'lb) petri_net \<Rig
 subsection \<open>Firing Sequence\<close>
 
 fun firing_sequence :: "('pl,'tr,'lb) petri_net \<Rightarrow> ('pl) markings \<Rightarrow> 'tr list \<Rightarrow> bool" where
-"firing_sequence pn m [] = True" |
-"firing_sequence pn m (tr#seq) = ((transition_enabled pn m tr) \<and> (firing_sequence pn (resulting_marking_firing_transition pn m tr) seq))"
+  "firing_sequence pn m [] = True" |
+  "firing_sequence pn m (tr#seq) = ((transition_enabled pn m tr) \<and> (firing_sequence pn (resulting_marking_firing_transition pn m tr) seq))"
 
 fun resulting_marking_firing_sequence :: "('pl,'tr,'lb) petri_net \<Rightarrow> ('pl) markings \<Rightarrow> 'tr list \<Rightarrow> ('pl) markings" where
-"resulting_marking_firing_sequence pn m [] = m" |
-"resulting_marking_firing_sequence pn m (tr#seq) = resulting_marking_firing_sequence pn (resulting_marking_firing_transition pn m tr) seq"
+  "resulting_marking_firing_sequence pn m [] = m" |
+  "resulting_marking_firing_sequence pn m (tr#seq) = resulting_marking_firing_sequence pn (resulting_marking_firing_transition pn m tr) seq"
 
 subsection \<open>Reachable Markings\<close>
 
@@ -66,35 +66,35 @@ lemma init_reachable_marking:
 subsection \<open>Pre and Post Sets\<close>
 
 definition transition_pre_set :: "('pl,'tr,'lb) petri_net \<Rightarrow> 'tr \<Rightarrow> 'pl set" where
-"transition_pre_set pn tr \<equiv> {pl. Pre pn tr pl > 0}"
+  "transition_pre_set pn tr \<equiv> {pl. Pre pn tr pl > 0}"
 
 definition transition_post_set :: "('pl,'tr,'lb) petri_net \<Rightarrow> 'tr \<Rightarrow> 'pl set" where
-"transition_post_set pn tr \<equiv> {pl. Post pn tr pl > 0}"
+  "transition_post_set pn tr \<equiv> {pl. Post pn tr pl > 0}"
 
 definition place_pre_set :: "('pl,'tr,'lb) petri_net \<Rightarrow> 'pl \<Rightarrow> 'tr set" where
-"place_pre_set pn pl \<equiv> {tr. Post pn tr pl > 0}"
+  "place_pre_set pn pl \<equiv> {tr. Post pn tr pl > 0}"
 
 definition place_post_set :: "('pl,'tr,'lb) petri_net \<Rightarrow> 'pl \<Rightarrow> 'tr set" where
-"place_post_set pn pl \<equiv> {tr. Pre pn tr pl > 0}"
+  "place_post_set pn pl \<equiv> {tr. Pre pn tr pl > 0}"
 
 subsection \<open>Observable Sequence\<close>
 
 fun observable_sequence :: "('tr,'lb) labellings \<Rightarrow> 'tr list \<Rightarrow> 'lb option list" where
-"observable_sequence lb [] = []" |
-"observable_sequence lb (tr#seq) = ((Rep_labellings lb tr)#(observable_sequence lb seq))"
+  "observable_sequence lb [] = []" |
+  "observable_sequence lb (tr#seq) = ((Rep_labellings lb tr)#(observable_sequence lb seq))"
 
 fun similar_observable_sequences :: "'lb option list \<Rightarrow> 'lb option list \<Rightarrow> bool" where
-"similar_observable_sequences [] [] = True" |
-"similar_observable_sequences (Some l1#seq1) (Some l2#seq2) = ((l1 = l2) \<and> (similar_observable_sequences seq1 seq2))" |
-"similar_observable_sequences (Some l1#seq1) [] = False" |
-"similar_observable_sequences [] (Some l2#seq2) = False" |
-"similar_observable_sequences (None#seq1) seq2 = similar_observable_sequences seq1 seq2" |
-"similar_observable_sequences seq1 (None#seq2) = similar_observable_sequences seq1 seq2"
+  "similar_observable_sequences [] [] = True" |
+  "similar_observable_sequences (Some l1#seq1) (Some l2#seq2) = ((l1 = l2) \<and> (similar_observable_sequences seq1 seq2))" |
+  "similar_observable_sequences (Some l1#seq1) [] = False" |
+  "similar_observable_sequences [] (Some l2#seq2) = False" |
+  "similar_observable_sequences (None#seq1) seq2 = similar_observable_sequences seq1 seq2" |
+  "similar_observable_sequences seq1 (None#seq2) = similar_observable_sequences seq1 seq2"
 
 subsection \<open>Compatible Markings\<close>
 
 definition compatible_markings :: "('pl,'tr,'lb) petri_net \<Rightarrow> ('pl) markings \<Rightarrow> ('pl,'tr,'lb) petri_net \<Rightarrow> ('pl) markings \<Rightarrow> bool" where
-"compatible_markings N1 m1 N2 m2 \<equiv> \<forall>p \<in> (P N1) \<inter> (P N2). (Rep_markings m1 p) = (Rep_markings m2 p)"
+  "compatible_markings N1 m1 N2 m2 \<equiv> \<forall>p \<in> (P N1) \<inter> (P N2). (Rep_markings m1 p) = (Rep_markings m2 p)"
 
 subsection \<open>Abstraction\<close>
 
@@ -106,33 +106,33 @@ record ('pl,'add_var) abstraction =
   system :: "('pl,'add_var) system_equations"
 
 definition abstraction_model :: "('pl,'add_var) abstraction \<Rightarrow>  ('pl) markings \<Rightarrow> ('pl) markings \<Rightarrow> bool" where
-"abstraction_model E m1 m2 \<equiv> (Rep_system_equations (system E)) (add_vars E) m1 m2"
+  "abstraction_model E m1 m2 \<equiv> (Rep_system_equations (system E)) (add_vars E) m1 m2"
 
 definition solvable :: "('pl,'add_var) abstraction \<Rightarrow> bool" where
-"solvable E \<equiv> (\<forall>m1. \<exists>m2. abstraction_model E m1 m2) 
-            \<and> (\<forall>m2. \<exists>m1. abstraction_model E m1 m2)"
+  "solvable E \<equiv> (\<forall>m1. \<exists>m2. abstraction_model E m1 m2) 
+              \<and> (\<forall>m2. \<exists>m1. abstraction_model E m1 m2)"
 
 subsection \<open>Markings Compatible With Abstraction\<close>
 
 definition markings_compatible_with_abstraction :: "('pl,'tr,'lb) petri_net \<Rightarrow> ('pl) markings \<Rightarrow> ('pl,'add_var) abstraction \<Rightarrow> ('pl,'tr,'lb) petri_net \<Rightarrow> ('pl) markings \<Rightarrow> bool" where
-"markings_compatible_with_abstraction N1 m1 E N2 m2 \<equiv> (compatible_markings N1 m1 N2 m2) \<and> (abstraction_model E m1 m2)"
+  "markings_compatible_with_abstraction N1 m1 E N2 m2 \<equiv> (compatible_markings N1 m1 N2 m2) \<and> (abstraction_model E m1 m2)"
 
 subsection \<open>E-Abstraction\<close>
 
 definition initial_markings_cond :: "('pl,'tr,'lb) petri_net \<Rightarrow> ('pl,'add_var) abstraction \<Rightarrow> ('pl,'tr,'lb) petri_net \<Rightarrow> bool" where
-"initial_markings_cond N1 E N2 \<equiv> markings_compatible_with_abstraction N1 (m0 N1) E N2 (m0 N2)"
+  "initial_markings_cond N1 E N2 \<equiv> markings_compatible_with_abstraction N1 (m0 N1) E N2 (m0 N2)"
 
 definition observable_sequences_cond :: "('pl,'tr,'lb) petri_net \<Rightarrow> ('pl,'add_var) abstraction \<Rightarrow> ('pl,'tr,'lb) petri_net \<Rightarrow> bool" where 
-"observable_sequences_cond N1 E N2 \<equiv> 
-  \<forall>tr1. (firing_sequence N1 (m0 N1) tr1)
-  \<longrightarrow> (\<forall>m2. (markings_compatible_with_abstraction N1 (resulting_marking_firing_sequence N1 (m0 N1) tr1) E N2 m2)
-      \<longrightarrow> (\<exists>tr2. (m2 = resulting_marking_firing_sequence N2 (m0 N2) tr2) \<and> (similar_observable_sequences (observable_sequence (l N1) tr1) (observable_sequence (l N2) tr2))))"
+  "observable_sequences_cond N1 E N2 \<equiv> 
+    \<forall>tr1. (firing_sequence N1 (m0 N1) tr1)
+    \<longrightarrow> (\<forall>m2. (markings_compatible_with_abstraction N1 (resulting_marking_firing_sequence N1 (m0 N1) tr1) E N2 m2)
+        \<longrightarrow> (\<exists>tr2. (m2 = resulting_marking_firing_sequence N2 (m0 N2) tr2) \<and> (similar_observable_sequences (observable_sequence (l N1) tr1) (observable_sequence (l N2) tr2))))"
 
 definition E_abstraction :: "('pl,'tr,'lb) petri_net \<Rightarrow> ('pl,'add_var) abstraction \<Rightarrow> ('pl,'tr,'lb) petri_net \<Rightarrow> bool" where
-"E_abstraction N1 E N2 \<equiv> (initial_markings_cond N1 E N2) \<and> (observable_sequences_cond  N1 E N2)"
+  "E_abstraction N1 E N2 \<equiv> (initial_markings_cond N1 E N2) \<and> (observable_sequences_cond  N1 E N2)"
 
 definition E_abstraction_equivalence :: "('pl,'tr,'lb) petri_net \<Rightarrow> ('pl,'ad_var) abstraction  \<Rightarrow> ('pl,'tr,'lb) petri_net \<Rightarrow> bool" where
-"E_abstraction_equivalence N1 E N2 \<equiv> (E_abstraction N1 E N2) \<and>  (E_abstraction N2 E N1)"
+  "E_abstraction_equivalence N1 E N2 \<equiv> (E_abstraction N1 E N2) \<and>  (E_abstraction N2 E N1)"
 
 
 end
